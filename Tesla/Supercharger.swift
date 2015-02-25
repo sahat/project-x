4084c10f693fe2ecd1531004a29b6e00ae91e790
Sahat Yalkabov <sakhat@gmail.com>
2015-02-24 20:43:54 -0800

    Created Supercharger struct

---
+++ Tesla/Supercharger.swift
@@ -0,0 +1,21 @@
+//
+//  Supercharger.swift
+//  Tesla
+//
+//  Created by Sahat Yalkabov on 2/24/15.
+//  Copyright (c) 2015 Sahat Yalkabov. All rights reserved.
+//
+
+import Foundation
+
+struct Supercharger {
+    let location: String
+    let address: String
+    let description: String
+    let state: String
+    let country: String
+    let latitude: Double
+    let longitude: Double
+    let slots: Int
+    let rate: Int
+}
